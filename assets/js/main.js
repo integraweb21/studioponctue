d38dc1479304aab64c0c370cc5e670240b5fba75
integraweb21 <52529402+integraweb21@users.noreply.github.com>
2025-09-10 17:52:25 +0200

    Correction animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,17 @@
-const p = document.querySelector('.animation p');
-p.innerHTML = p.innerHTML + " | " + p.innerHTML;
+document.querySelectorAll('.outer').forEach(el => {
+    let content = el.querySelector('.content');
+
+    repeatContent(content, el.offsetWidth);
+
+    let slider = el.querySelector('.loop');
+    slider.innerHTML = slider.innerHTML + slider.innerHTML;
+});
+
+function repeatContent(el, till) {
+    let html = el.innerHTML;
+    let counter = 0;
+    while (el.offsetWidth < till && counter < 100) {
+        el.innerHTML += html;
+        counter += 1;
+    }
+}
